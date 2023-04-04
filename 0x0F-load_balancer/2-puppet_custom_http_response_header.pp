52a5d71b47fef1de6265241e8d0f4f332b81614b
H3c7o4 <hectorvladitok@gmail.com>
2023-04-04 11:24:25 +0200

    Updating all the other files

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,38 +1,17 @@
-# Script that setup a nginx web server on our server + redirection + header
-exec { 'update':
-  command => '/usr/bin/apt-get update',
+# Installs Nginx and custom HTTP response headers
+exec { '/usr/bin/env apt-get -y update' : }
+-> package { 'nginx':
+  ensure => installed,
 }
-
-package { 'nginx':
-  ensure  => installed,
-  name    => 'nginx',
-  require => Exec['apt-get-update'],
+-> file { '/var/www/html/index.html' :
+  content => 'Holberton School!',
 }
-
-file { '/var/www/html/index.html':
-  ensure  => 'present',
-  path    => '/var/www/html/index.html',
-  content => 'Hello World!',
-  require => Package['nginx'],
+-> file_line { 'add header' :
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "\tadd_header X-Served-By ${hostname};",
+  after  => 'server_name _;',
 }
-
-file_line { 'redirect_me':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'listen 80 default_server;',
-  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-  require => Package['nginx'],
-}
-
-file_line { 'addHeader':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'listen 80 default_server;',
-  line    => 'add_header X-Served-By $hostname;',
-  require => Package['nginx'],
-}
-
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+-> service { 'nginx':
+  ensure => running,
 }
